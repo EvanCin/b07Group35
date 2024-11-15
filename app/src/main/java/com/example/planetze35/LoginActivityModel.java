eab71e9c65e690d374d2dd71902d69143355681d
FlappyAzar <sepiazad1111@gmail.com>
2024-11-15 01:37:42 -0500

    refactor: remove unnecessary feature greetUser

--- app/src/main/java/com/example/planetze35/LoginActivityModel.java
+++ app/src/main/java/com/example/planetze35/LoginActivityModel.java
@@ -1,18 +1,10 @@
 package com.example.planetze35;
 
 import android.graphics.Color;
-import android.widget.Toast;
-
-import androidx.annotation.NonNull;
 
 import com.google.android.material.snackbar.Snackbar;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
 
 public class LoginActivityModel {
 
@@ -41,8 +33,7 @@ public class LoginActivityModel {
                 // check if the email is verified
                 if (user.isEmailVerified()) {
                     // TODO: send user to the welcome activity if it is their first time logging in
-                    // greet user and send them to the home page
-                    greetUser(presenter, user);
+                    // send them to the home page
                     presenter.navigateToMainActivity();
                 } else {
                     // send user to the email verification page to verify their email
@@ -55,24 +46,4 @@ public class LoginActivityModel {
         });
     }
 
-    /**
-     * Get the user's first name and call the appropriate method in the presenter to greet the user
-     * @param user The user
-     */
-    private void greetUser(LoginActivityPresenter presenter, FirebaseUser user) {
-        String uid = user.getUid();
-        DatabaseReference dbRef = FirebaseDatabase.getInstance().getReference("users").child(uid);
-        dbRef.child("firstName").addListenerForSingleValueEvent(new ValueEventListener() {
-            @Override
-            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                // handle the user's first name
-                presenter.setToast("Hello " + dataSnapshot.getValue(String.class), Toast.LENGTH_LONG);
-            }
-
-            @Override
-            public void onCancelled(@NonNull DatabaseError databaseError) {
-                presenter.setToast("Error: " + databaseError.getMessage(), Toast.LENGTH_LONG);
-            }
-        });
-    }
 }
