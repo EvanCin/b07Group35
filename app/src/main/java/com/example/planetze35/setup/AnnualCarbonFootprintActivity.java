e0b701604b737c6a176eb627eedd6fcd11d7920a
Evan <111394939+EvanCin@users.noreply.github.com>
2024-11-28 00:54:07 -0500

    feat:data from csv now shows in spinner and chosen country is stored in firebase

--- app/src/main/java/com/example/planetze35/setup/AnnualCarbonFootprintActivity.java
+++ app/src/main/java/com/example/planetze35/setup/AnnualCarbonFootprintActivity.java
@@ -3,6 +3,7 @@ package com.example.planetze35.setup;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -10,6 +11,11 @@ import android.widget.Button;
 import android.widget.ListView;
 import android.widget.Spinner;
 import android.widget.TextView;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 
 import androidx.activity.EdgeToEdge;
@@ -19,12 +25,19 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 import com.example.planetze35.R;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class AnnualCarbonFootprintActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
     Spinner spinner;
     ListViewCF listViewCF;
     ArrayList<Integer> selectedChoices = new ArrayList<>();
     Button nextPageButton;
+    ArrayList<String> countries;
+    String selectedCountry;
+    DatabaseReference mDatabase;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,14 +49,11 @@ public class AnnualCarbonFootprintActivity extends AppCompatActivity implements 
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        readCSV();
         spinner = findViewById(R.id.spinner);
-        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(
-                this,
-                R.array.countries,
-                android.R.layout.simple_spinner_item
-        );
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        spinner.setAdapter(adapter);
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item,countries);
+        arrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(arrayAdapter);
         spinner.setOnItemSelectedListener(this);
 
         listViewCF = new ListViewCF(findViewById(R.id.listView),0);
@@ -53,7 +63,7 @@ public class AnnualCarbonFootprintActivity extends AppCompatActivity implements 
             @Override
             public void onClick(View v) {
                 Intent intent;
-                if(listViewCF.getListView().getCheckedItemPosition() == AdapterView.INVALID_POSITION) return;
+                if(listViewCF.getListView().getCheckedItemPosition() == AdapterView.INVALID_POSITION || selectedCountry == null) return;
                 selectedChoices.add(listViewCF.getListView().getCheckedItemPosition());
                 if(selectedChoices.get(0) == 0) {
                     intent = new Intent(AnnualCarbonFootprintActivity.this, TransportationC1Activity.class);
@@ -64,15 +74,38 @@ public class AnnualCarbonFootprintActivity extends AppCompatActivity implements 
                     selectedChoices.add(-1);
                 }
                 intent.putExtra("selectedChoices", selectedChoices);
+                //Temporarily store data to firebase using dummy user
+                FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+                assert user != null;
+                String uid = user.getUid();
+                mDatabase = FirebaseDatabase.getInstance().getReference("users").child(uid);
+                mDatabase.child("country").setValue(selectedCountry);
                 startActivity(intent);
             }
         });
     }
 
+    public void readCSV() {
+        countries = new ArrayList<>();
+        InputStream stream = AnnualCarbonFootprintActivity.this.getResources()
+                .openRawResource(R.raw.global_averages);
+        BufferedReader br = new BufferedReader(new InputStreamReader(stream));
+        try {
+            String line = br.readLine();
+            line = br.readLine();
+            while(line != null) {
+                countries.add(line.split(",")[0]);
+                line = br.readLine();
+            }
+        } catch(IOException ioException) {
+            Log.d("AnnualCarbonFootprintActivity", "IOException");
+        }
+    }
+
     @Override
     public void onItemSelected(AdapterView<?> parent, View view,
                                int pos, long id) {
-
+        selectedCountry = countries.get(pos);
     }
     @Override
     public void onNothingSelected(AdapterView<?> parent) {
