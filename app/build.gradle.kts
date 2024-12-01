faddf232b3bb649b8e6e34bd807292ed96e93f53
Lunabertha <lunabertha48@gmail.com>
2024-12-01 17:41:25 -0500

    Minor changes

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -34,13 +34,6 @@ android {
 }
 
 dependencies {
-    implementation(platform(libs.firebase.bom))
-    implementation(libs.firebase.database)
-    implementation(libs.firebase.auth)
-    implementation(libs.firebase.analytics)
-    implementation(libs.firebase.firestore)
-
-
 
     implementation(libs.appcompat)
     implementation(libs.material)
