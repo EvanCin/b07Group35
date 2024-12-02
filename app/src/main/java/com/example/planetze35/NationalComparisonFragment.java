7674d7ed585c97903d624de888a4916d7f1f6b1a
Jeffery Gao <greennature207@gmail.com>
2024-12-01 19:23:32 -0500

    feat: NationalComparisonFragment gets user's country from database

--- app/src/main/java/com/example/planetze35/NationalComparisonFragment.java
+++ app/src/main/java/com/example/planetze35/NationalComparisonFragment.java
@@ -14,19 +14,29 @@ import android.view.ViewGroup;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.io.IOException;
-import java.util.HashMap;
 
 public class NationalComparisonFragment extends Fragment {
-    TextView gaugeAvgComparisonNationalUserEmissions;
-    TextView gaugeAvgComparisonNationalAvgHeader;
-    TextView gaugeAvgComparisonNationalAvgEmissions;
+    private TextView gaugeAvgComparisonNationalUserEmissions;
+    private TextView gaugeAvgComparisonNationalAvgHeader;
+    private TextView gaugeAvgComparisonNationalAvgEmissions;
+    private FirebaseUser currentUser;
+    private String uid;
+    private DatabaseReference dbUserRef;
+    private DatabaseReference dbUserCountryRef;
+    private ValueEventListener countryGetterListener;
+    private String country;
 
-    public NationalComparisonFragment() {}
+    public NationalComparisonFragment() {
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -37,26 +47,70 @@ public class NationalComparisonFragment extends Fragment {
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        Context context = view.getContext();
+
         gaugeAvgComparisonNationalUserEmissions = view.findViewById(R.id.gaugeAvgComparisonNationalUserEmissions);
         gaugeAvgComparisonNationalAvgHeader = view.findViewById(R.id.gaugeAvgComparisonNationalAvgHeader);
         gaugeAvgComparisonNationalAvgEmissions = view.findViewById(R.id.gaugeAvgComparisonNationalAvgEmissions);
 
-        Context context = view.getContext();
+        // Get database node containing current user's data
+        currentUser = FirebaseAuth.getInstance().getCurrentUser();
+        uid = (currentUser != null) ? currentUser.getUid() : "defaultUserId";
+        dbUserRef = FirebaseDatabase.getInstance().getReference().child("users/" + uid);
+        Log.d("NationalComparisonFragment", "Current user ID: " + uid);
 
-        // TODO: Actually get the country of the user here once that is implemented
-        String country = "Canada";
-        gaugeAvgComparisonNationalAvgHeader.setText("The average in " + country + " is");
-
-        try {
-            AvgEmissionModel model = AvgEmissionModel.getInstance(context);
-            String countryEmissionAvgTxt = String.format("%.2f", model.getAvgEmission(country));
-            gaugeAvgComparisonNationalAvgEmissions.setText(countryEmissionAvgTxt);
-        } catch (IOException e) {
-            Toast.makeText(context,
-                    "Could not access local file containing global average emissions.",
-                    Toast.LENGTH_SHORT).show();
-        }
+        updateNationalAvgEmissionDisplay(context);
 
         // TODO: Get the user's emission average once that is implemented
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        dbUserCountryRef.removeEventListener(countryGetterListener);
+    }
+
+    public void updateNationalAvgEmissionDisplay(Context context) {
+        country = "???";
+        dbUserCountryRef = dbUserRef.child("country");
+        countryGetterListener = new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                Object o = snapshot.getValue();
+                if (o instanceof String) {
+                    country = (String) o;
+                    Log.d("NationalComparisonFragment",
+                            "Current user's country: " + country);
+                    gaugeAvgComparisonNationalAvgHeader.setText("The average in " + country + " is");
+
+                    try {
+                        AvgEmissionModel model = AvgEmissionModel.getInstance(context);
+                        String countryEmissionAvgTxt = !country.equals("???")
+                                ? String.format("%.2f", model.getAvgEmission(country)) : country;
+                        gaugeAvgComparisonNationalAvgEmissions.setText(countryEmissionAvgTxt);
+                    } catch (IOException e) {
+                        Toast.makeText(context,
+                                "Could not access local file containing global average emissions.",
+                                Toast.LENGTH_SHORT).show();
+                    }
+                } else {
+                    Toast.makeText(context,
+                            "Could not get the user's country from the database.",
+                            Toast.LENGTH_SHORT).show();
+                    Log.e("NationalComparisonFragment",
+                            "Failed to get country of user (in onDataChange)");
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                Toast.makeText(context,
+                        "Could not get the user's country from the database.",
+                        Toast.LENGTH_SHORT).show();
+                Log.e("NationalComparisonFragment",
+                        "Failed to get country of user (in onCancelled)", error.toException());
+            }
+        };
+        dbUserCountryRef.addListenerForSingleValueEvent(countryGetterListener);
     }
 }
