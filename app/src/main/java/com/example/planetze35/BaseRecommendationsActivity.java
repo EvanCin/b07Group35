52c326ebf8c81a0c81f19842d41e95d875d70aef
Lunabertha <lunabertha48@gmail.com>
2024-12-02 19:57:34 -0500

    Modified BaseRecommendationsActivity.java

--- app/src/main/java/com/example/planetze35/BaseRecommendationsActivity.java
+++ app/src/main/java/com/example/planetze35/BaseRecommendationsActivity.java
@@ -10,7 +10,6 @@ import android.widget.Button;
 import android.widget.SearchView;
 
 import androidx.annotation.Nullable;
-import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
